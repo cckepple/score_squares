c1ce64c302a2d30b66d1d67a9ca7f0f03d9f8753
Corey Kepple <corey@parlevelsystems.com>
2016-02-03 21:48:24 -0600

    Fixed issue with admin panel

--- resources/views/pool/show.blade.php
+++ resources/views/pool/show.blade.php
@@ -92,6 +92,7 @@ app.controller('ShowPoolCtrl', function ($scope, $http, $filter, $location, $tim
 			$scope.gameInfo = data.gameInfo;
 			$scope.admin = data.admin;
 			$scope.grid = []
+			console.log
 			var i = 0;
 			for (var r = 0; r < 10; r++) {
 				$scope.grid.push({'row':r+1, 'slots':[]});
@@ -406,7 +407,7 @@ app.controller('RemovePayModalInstanceCtrl', function ($scope, $uibModalInstance
 						  		<div class="btn-group text-center" role="group">
 									<button class="btn btn-primary" ng-class="{'active': divs[0].active}" ng-click="showDiv(1)">Squares</button>
 									<button class="btn btn-primary" ng-class="{'active': divs[1].active}" ng-click="showDiv(2)">Scores</button>
-									<button class="btn btn-primary" ng-class="{'active': divs[2].active}" ng-click="showDiv(3)" ng-show="curUser == admin.user.id">Admin</button>
+									<button class="btn btn-primary" ng-class="{'active': divs[2].active}" ng-click="showDiv(3)" ng-show="myId == admin.user.id">Admin</button>
 								</div>	
 						  	</div>
 					  	</div>
